e8e3f860e87acddfefd6254dc5db7f93a8b54884
Komarova <posja2007@gmail.com>
2025-04-07 11:29:15 +0300

    Parāda lielāko skaitlis

--- src/kaulins.java
+++ src/kaulins.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 
 public class kaulins {
 	static void mestKaulinu(int reiz) {
-		int sk, reizes;
+		int sk, reizes, lielakais = 0;
 		Random rand =  new Random();
 		for(int i=0; i<reiz; i++) {
 			sk = rand.nextInt(6)+1;
 			System.out.println("Skaitlis uzkrita: " +sk);
+			if(lielakais<sk)
+				lielakais = sk;
 		}
+		System.out.println("Lielākais skaitlis no visiem izmestajiem: "+lielakais);
 	}
 	//sveiki
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int reizes;
+		do {
 		System.out.println("Cik reizes mest kaulinu? ");
-		int reizes = scan.nextInt();
+		reizes = scan.nextInt();
+		}while(reizes < 1);
 		scan.close();
 		mestKaulinu(reizes);
 	}
